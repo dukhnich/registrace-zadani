eec55c36f8ef62163ed099377c491ca482fbb7f3
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2024-06-13 21:06:15 +0200

    refactor with Formik

--- src/components/Registration/index.tsx
+++ src/components/Registration/index.tsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import { useState } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 
 interface RegistrationData {
   username: string;
@@ -14,6 +14,7 @@ interface inputData {
   type: inputsNames;
   inputType: string;
   label: string;
+  validation: (str: string, allValues: RegistrationData) => string | boolean;
 }
 
 const inputsArr: inputData[] = [
@@ -21,65 +22,96 @@ const inputsArr: inputData[] = [
     type: "email",
     inputType: "email",
     label: "Email Address",
+    validation: (str) => {
+      if (!str) {
+        return "Povinné pole";
+      }
+      return str.indexOf("@") === -1 ? "Musí mít @" : true;
+    },
   },
   {
     type: "username",
     inputType: "text",
     label: "User Name",
+    validation: (str) => {
+      if (!str) {
+        return "Povinné pole";
+      }
+      return str.length > 15 ? "Musí být dlouhé maximálně 15 znaků" : true;
+    },
   },
   {
     type: "password",
     inputType: "password",
     label: "Password",
+    validation: (str) => {
+      if (!str) {
+        return "Povinné pole";
+      }
+      return str.length < 6 || str.length > 15
+        ? "Musí být dlouhé mezi 6 a 15 znaků"
+        : true;
+    },
   },
   {
     type: "passwordConfirm",
     inputType: "password",
     label: "Confirm Password",
+    validation: (str, values) => {
+      if (!str) {
+        return "Povinné pole";
+      }
+      return str !== values.password ? "Heslo musí být stejné" : true;
+    },
   },
 ];
 
 export const Registration: React.FC = () => {
-  const [user, setUser] = useState<RegistrationData>({
-    username: "",
-    email: "",
-    password: "",
-    passwordConfirm: "",
-  });
-
-  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    console.log(user);
+  const onSubmit = (data: RegistrationData) => {
+    console.log(data);
   };
-  const changeField = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const updated = {
-      ...user,
-      [e.target.name]: e.target.value,
-    };
-    if (e.target.name === "email" && !updated.username) {
-      const index = e.target.value.indexOf("@");
-      if (index !== -1) {
-        updated.username = e.target.value.slice(0, index);
+  const validate = (values: RegistrationData) => {
+    const errors: Record<string, string> = {};
+    Object.entries(values).forEach(([key, value]) => {
+      const validator = inputsArr.find((i) => i.type === key)?.validation;
+      if (!validator) {
+        return;
       }
-    }
-    setUser(updated);
+      const result = validator(value, values);
+      if (typeof result === "string") {
+        errors[key] = result;
+      }
+    });
+    return errors;
   };
   return (
-    <form onSubmit={onSubmit} className="registration">
-      {inputsArr.map((input) => (
-        <input
-          key={input.type}
-          type={input.inputType}
-          className={`registration__input registration__input--${input.type}`}
-          name={input.type}
-          value={user[input.type]}
-          placeholder={input.label}
-          onChange={changeField}
-        />
-      ))}
-      <button type="submit" className="registration__btn">
-        Register
-      </button>
-    </form>
+    <Formik
+      className="registration"
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      }}
+      validate={validate}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        {inputsArr.map((input) => (
+          <div key={input.type}>
+            <label htmlFor={input.type}>{input.label}</label>
+            <Field
+              type={input.inputType}
+              className={`registration__input registration__input--${input.type}`}
+              name={input.type}
+            />
+            <ErrorMessage name={input.type} />
+          </div>
+        ))}
+        <button type="submit" className="registration__btn">
+          Register
+        </button>
+      </Form>
+    </Formik>
   );
 };
